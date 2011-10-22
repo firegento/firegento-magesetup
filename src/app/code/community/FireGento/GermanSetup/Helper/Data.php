ef5100ea3f515d610a75034693eaf73ee9f5f727
Michael Türk <tuerk@flagbit.de>
2011-10-22 23:55:46 +0200

    [+TASK] Added PHP file and class comments to all existing PHP files

--- src/app/code/community/FireGento/GermanSetup/Helper/Data.php
+++ src/app/code/community/FireGento/GermanSetup/Helper/Data.php
@@ -1,7 +1,35 @@
 <?php
+/**                                                                       
+ * This file is part of the FIREGENTO project.
+ * 
+ * FireGento_GermanSetup is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License version 3 as 
+ * published by the Free Software Foundation.
+ * 
+ * This script is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * PHP version 5
+ *
+ * @category  FireGento
+ * @package   FireGento_GermanSetup
+ * @author    FireGento Team <team@firegento.com>
+ * @copyright 2011 FireGento Team (http://www.firegento.de). All rights served.
+ * @license   http://opensource.org/licenses/gpl-3.0 GNU General Public License, version 3 (GPLv3)
+ * @version   $Id:$
+ * @since     0.1.0
+ */
 /**
- * @package     FireGento
- * @subpackage  FireGento_GermanSetup
+ * Dummy data helper for translation issues.
+ *
+ * @category  FireGento
+ * @package   FireGento_GermanSetup
+ * @author    FireGento Team <team@firegento.com>
+ * @copyright 2011 FireGento Team (http://www.firegento.de). All rights served.
+ * @license   http://opensource.org/licenses/gpl-3.0 GNU General Public License, version 3 (GPLv3)
+ * @version   $Id:$
+ * @since     0.1.0
  */
 class FireGento_GermanSetup_Helper_Data extends Mage_Core_Helper_Abstract
 {
