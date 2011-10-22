aa8623b9e488a456ae2b50d51e6e79d113448e4e
Andreas von Studnitz <avs@avs-webentwicklung.de>
2011-10-22 23:30:15 +0200

    [+FEATURE] Added static block below price block

---
+++ src/app/code/community/FireGento/GermanSetup/Block/Catalog/ProductPrice.php
@@ -0,0 +1,27 @@
+<?php
+class FireGento_GermanSetup_Block_Catalog_ProductPrice extends Mage_Catalog_Block_Product_Price
+{
+    /**
+     * Add content of static block below price html if defined in config
+     *
+     * @return string
+     */
+    public function _toHtml()
+    {
+        $html = parent::_toHtml();
+
+        $blockIdentifier = Mage::getStoreConfig('catalog/price/block_below_price');
+
+        if ($blockIdentifier) {
+            $blockModel = Mage::getModel('cms/block')
+                ->setStoreId(Mage::app()->getStore()->getId())
+                ->load($blockIdentifier);
+
+            if ($blockModel->getId() && $blockModel->getIsActive()) {
+                $html .= $this->getLayout()->createBlock('cms/block')->setBlockId($blockModel->getId())->toHtml();
+            }
+
+        }
+        return $html;
+    }
+}
