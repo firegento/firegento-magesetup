7caa42054012aaaba17e6f6a10c3fb25c5df6c55
therouv <therouv@googlemail.com>
2011-10-23 11:18:42 +0200

    [+FEATURE] Added filter to checkout agreements in an observer

--- src/app/code/community/FireGento/GermanSetup/Model/Observer.php
+++ src/app/code/community/FireGento/GermanSetup/Model/Observer.php
@@ -1,12 +1,75 @@
 <?php
-
+/**
+ * This file is part of the FIREGENTO project.
+ *
+ * FireGento_GermanSetup is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This script is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * PHP version 5
+ *
+ * @category  FireGento
+ * @package   FireGento_GermanSetup
+ * @author    FireGento Team <team@firegento.com>
+ * @copyright 2011 FireGento Team (http://www.firegento.de). All rights served.
+ * @license   http://opensource.org/licenses/gpl-3.0 GNU General Public License, version 3 (GPLv3)
+ * @version   $Id:$
+ * @since     0.2.0
+ */
+/**
+ * Observer class
+ *
+ * @category  FireGento
+ * @package   FireGento_GermanSetup
+ * @author    FireGento Team <team@firegento.com>
+ * @copyright 2011 FireGento Team (http://www.firegento.de). All rights served.
+ * @license   http://opensource.org/licenses/gpl-3.0 GNU General Public License, version 3 (GPLv3)
+ * @version   $Id:$
+ * @since     0.2.0
+ */
 class FireGento_GermanSetup_Model_Observer
 {
+    /**
+     * Filters all agreements
+     *
+     * Filters all agreements against the Magento template filter. This enables the Magento
+     * administrator define a cms static block as the content of the checkout agreements..
+     *
+     * @param Varien_Event_Observer $observer Observer
+     * @return FireGento_GermanSetup_Model_Observer Self.
+     */
     public function filterAgreements(Varien_Event_Observer $observer)
     {
-        $object = $observer->getEvent()->getObject();
-        if ($object instanceof Mage_Checkout_Model_Agreement) {
-            Mage::helper('firegento/log')->firelogger($object);
+        $block = $observer->getEvent()->getBlock();
+        if ($block->getType() == 'checkout/agreements') {
+            if ($agreements = $block->getAgreements()) {
+                $collection = new Varien_Data_Collection();
+                foreach ($agreements as $agreement) {
+                    $agreement->setData('content', $this->_filterString($agreement->getData('content')));
+                    $agreement->setData('checkbox_text', $this->_filterString($agreement->getData('checkbox_text')));
+                    $collection->addItem($agreement);
+                }
+                $observer->getEvent()->getBlock()->setAgreements($collection);
+            }
         }
+        return $this;
+    }
+
+    /**
+     * Calls the Magento template filter to transform {{block type="cms/block" block_id="XXX"}}
+     * into the specific html code
+     * 
+     * @param string $string Agreement to filter
+     * @return string Processed String
+     */
+    protected function _filterString($string)
+    {
+        $processor = Mage::getModel('cms/template_filter');
+        $html      = $processor->filter($string);
+        return $html;
     }
 }
