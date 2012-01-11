6c76461a8736387d23ddf2e29b161cb7883157fa
therouv <therouv@googlemail.com>
2012-01-11 13:26:09 +0100

    Autogeneration of meta information

--- src/app/code/community/FireGento/GermanSetup/Model/Observer.php
+++ src/app/code/community/FireGento/GermanSetup/Model/Observer.php
@@ -34,6 +34,20 @@
 class FireGento_GermanSetup_Model_Observer
 {
     /**
+     * Calls the Magento template filter to transform {{block type="cms/block" block_id="XXX"}}
+     * into the specific html code
+     *
+     * @param string $string Agreement to filter
+     * @return string Processed String
+     */
+    protected function _filterString($string)
+    {
+        $processor = Mage::getModel('cms/template_filter');
+        $string    = $processor->filter($string);
+        return $string;
+    }
+
+    /**
      * Auto-Generates the meta information of a product.
      *
      * @param Varien_Event_Observer $observer Observer
@@ -41,8 +55,9 @@ class FireGento_GermanSetup_Model_Observer
      */
     public function autogenerateMetaInformation(Varien_Event_Observer $observer)
     {
-        /** @var $product Mage_Catalog_Model_Product */
+        /* @var $product Mage_Catalog_Model_Product */
         $product = $observer->getEvent()->getProduct();
+
         if ($product->getData('meta_autogenerate') == 1) {
             // Set Meta Title
             $product->setMetaTitle($product->getName());
@@ -99,53 +114,63 @@ class FireGento_GermanSetup_Model_Observer
     }
 
     /**
-     * Calls the Magento template filter to transform {{block type="cms/block" block_id="XXX"}}
-     * into the specific html code
-     * 
-     * @param string $string Agreement to filter
-     * @return string Processed String
-     */
-    protected function _filterString($string)
-    {
-        $processor = Mage::getModel('cms/template_filter');
-        $string    = $processor->filter($string);
-        return $string;
-    }
-
-    /**
      * Get the categories of the current product
-     * 
+     *
      * @param Mage_Catalog_Model_Product $product Product
      * @return array Categories
      */
     protected function _getCategories($product)
     {
         $categories = $product->getCategoryIds();
-        $categoryArr = $this->_loadCategoriesRecursive($categories);
-        Zend_Debug::dump($categoryArr);
+        $categoryArr = $this->_fetchCategoryNames($categories);
         $keywords = $this->_buildKeywords($categoryArr);
         return $keywords;
     }
 
     /**
-     * Loads the categories recursice bottom-up to root-categories
-     * 
+     * Fetches all category names via category path; adds first the assigned
+     * categories and second all categories via path.
+     *
      * @param array $categories Category Ids
      * @param int   $level      Current level
      * @return array Categories
      */
-    protected function _loadCategoriesRecursive($categories, $level=0)
+    protected function _fetchCategoryNames($categories)
     {
-        $return = array();
+        $return = array(
+            'assigned' => array(),
+            'path'     => array()
+        );
+
         foreach ($categories as $categoryId) {
-            /** @var $category Mage_Catalog_Model_Category */
-            $category = Mage::getModel('catalog/category')
-                ->setStoreId()
-                ->load($categoryId);
-            $return[$level][$categoryId] = $category->getName();
-            if (!in_array($category->getParentId(), array(1, 2))) {
-                $tmp = $this->_loadCategoriesRecursive(array($category->getParentId()), $level+1);
-                $return[$level+1][$category->getParentId()] = $tmp[$level+1][$category->getParentId()];
+            // Check if category was already added
+            if (array_key_exists($categoryId, $return['assigned'])
+                || array_key_exists($categoryId, $return['path'])
+            ) {
+                return;
+            }
+
+            /* @var $category Mage_Catalog_Model_Category */
+            $category = Mage::getModel('catalog/category')->load($categoryId);
+            $return['assigned'][$categoryId] = $category->getName();
+
+            // Fetch path ids and remove the first two (base and root category)
+            $path    = $category->getPath();
+            $pathIds = explode('/', $path);
+            array_shift($pathIds);
+            array_shift($pathIds);
+
+            // Fetch the names from path categories
+            if (count($pathIds) > 0) {
+                foreach ($pathIds as $pathId) {
+                    if (!array_key_exists($pathId, $return['assigned'])
+                        && !array_key_exists($pathId, $return['path'])
+                    ) {
+                        /* @var $pathCategory Mage_Catalog_Model_Category */
+                        $pathCategory = Mage::getModel('catalog/category')->load($pathId);
+                        $return['path'][$pathId] = $pathCategory->getName();
+                    }
+                }
             }
         }
         return $return;
@@ -153,7 +178,7 @@ class FireGento_GermanSetup_Model_Observer
 
     /**
      * Processes the category array and generates a string
-     * 
+     *
      * @param array $categories Categories
      * @return string Keywords
      */
@@ -161,8 +186,8 @@ class FireGento_GermanSetup_Model_Observer
     {
         $keywords  = '';
         $processed = array();
-        foreach ($categories as $level => $categoryNames) {
-            foreach ($categoryNames as $catId => $cat) {
+        foreach ($categories as $cats) {
+            foreach ($cats as $catId => $cat) {
                 if (!in_array($catId, $processed)) {
                     $keywords .= $cat.', ';
                     $processed[] = $catId;
