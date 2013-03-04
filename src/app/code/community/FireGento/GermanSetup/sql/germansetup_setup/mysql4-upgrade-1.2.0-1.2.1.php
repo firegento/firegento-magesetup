07d15447fc3ee9caad657bd8b136046e26dc6710
Andreas von Studnitz <avs@integer-net.de>
2013-03-04 11:34:10 +0100

    [+FEATURE] Trusted Shops: Added possibility to display product weight with product prices

---
+++ src/app/code/community/FireGento/GermanSetup/sql/germansetup_setup/mysql4-upgrade-1.2.0-1.2.1.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * This file is part of the FIREGENTO project.
+ *
+ * FireGento_GermanSetup is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This script is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * PHP version 5
+ *
+ * @category  FireGento
+ * @package   FireGento_GermanSetup
+ * @author    FireGento Team <team@firegento.com>
+ * @copyright 2012 FireGento Team (http://www.firegento.de). All rights served.
+ * @license   http://opensource.org/licenses/gpl-3.0 GNU General Public License, version 3 (GPLv3)
+ * @version   $Id:$
+ * @since     0.1.0
+ */
+/**
+ * Setup script; Adds the delivery_time attribute for products
+ *
+ * @category  FireGento
+ * @package   FireGento_GermanSetup
+ * @author    FireGento Team <team@firegento.com>
+ * @copyright 2012 FireGento Team (http://www.firegento.de). All rights served.
+ * @license   http://opensource.org/licenses/gpl-3.0 GNU General Public License, version 3 (GPLv3)
+ * @version   $Id:$
+ * @since     1.2.1
+ */
+
+/** @var $installer Mage_Catalog_Model_Resource_Eav_Mysql4_Setup */
+$installer = $this;
+$installer->startSetup();
+
+// Update attribute properties
+$installer->updateAttribute(
+    'catalog_product',
+    'weight',
+    'used_in_product_listing',
+    true
+);
+
+$installer->endSetup();
