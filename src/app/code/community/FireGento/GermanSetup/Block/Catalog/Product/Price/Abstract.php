eeaa36b39def4f4a48b9c4dfc9acc94ccc4290a8
Andreas von Studnitz <avs@integer-net.de>
2013-03-26 22:42:14 +0100

    rewrite sitewards_b2b only up to version 2.1.0 - later on it uses events to deal with that

--- src/app/code/community/FireGento/GermanSetup/Block/Catalog/Product/Price/Abstract.php
+++ src/app/code/community/FireGento/GermanSetup/Block/Catalog/Product/Price/Abstract.php
@@ -33,7 +33,10 @@
  * @since     0.1.0
  */
 
-if (Mage::getConfig()->getModuleConfig('Sitewards_B2BProfessional')->is('active', 'true')) {
+if (
+    Mage::getConfig()->getModuleConfig('Sitewards_B2BProfessional')->is('active', 'true')
+    && version_compare (Mage::getConfig()->getModuleConfig('Sitewards_B2BProfessional')->version, '2.1.0', '<=' )
+) {
 
     abstract class FireGento_GermanSetup_Block_Catalog_Product_Price_Abstract
         extends Sitewards_B2BProfessional_Block_Price
