a884eee41c590ca0a1b7bb9150742e2f546ba752
Rokko11 <richardvogel123@gmx.de>
2013-10-18 07:51:47 +0200

    Rewrite of Ga-Block using core_block_abstract_to_html_after-Event

---
+++ src/app/code/community/FireGento/MageSetup/Model/Ga/Observer.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Richard.Vogel
+ * Date: 18.10.13
+ * Time: 07:22
+ * To change this template use File | Settings | File Templates.
+ */
+
+class FireGento_MageSetup_Model_Ga_Observer
+{
+    public function appendAccountInformation(Varien_Event_Observer $observer)
+    {
+        if (($block = $observer->getEvent()->getBlock()) instanceof Mage_GoogleAnalytics_Block_Ga) {
+            $transport = $observer->getEvent()->getTransport();
+
+            if (!$transport || !($html = $transport->getHtml()) || !Mage::getStoreConfigFlag(self::CONFIG_GOOGLE_ANALYTICS_IP_ANONYMIZATION)) {
+                return $this;
+            }
+
+            $matches = array();
+            $setAccountExpression = '/_gaq\.push\(\[\'_setAccount\', \'[a-zA-Z0-9-_]+\'\]\);\n/';
+            $append = '_gaq.push([\'_gat._anonymizeIp\']);';
+
+            if (preg_match_all($setAccountExpression, $html, $matches) && count($matches) && count($matches[0])) {
+                $html = preg_replace($setAccountExpression, $matches[0][0] . $append . "\n", $html);
+                $transport->setHtml($html);
+            }
+        }
+        return $this;
+    }
+}
