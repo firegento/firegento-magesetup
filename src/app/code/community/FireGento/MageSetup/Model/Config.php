107f83f02b27c53802d755487cb81434d32d00ad
Andreas von Studnitz <avs@integer-net.de>
2015-02-20 08:30:11 +0100

    [TASK] Version bump

--- src/app/code/community/FireGento/MageSetup/Model/Config.php
+++ src/app/code/community/FireGento/MageSetup/Model/Config.php
@@ -17,7 +17,7 @@
  * @author    FireGento Team <team@firegento.com>
  * @copyright 2013-2015 FireGento Team (http://www.firegento.com)
  * @license   http://opensource.org/licenses/gpl-3.0 GNU General Public License, version 3 (GPLv3)
- * @version   2.2.1
+ * @version   2.2.2
  * @since     0.2.0
  */
 
