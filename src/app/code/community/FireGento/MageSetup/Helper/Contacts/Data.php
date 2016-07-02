34931342ff245f2612100a0ca0905b6af5d89507
Julian Wittorf <jwittorf@die-netzwerkstatt.de>
2016-07-02 19:58:52 +0200

    [+FEATURE] Add checkbox to default contacts form to accept agreement
    Is required by German law due to a ruling by OLG Köln from 11.03.2016
    Is disabled by default, needs to be set for German shops

---
+++ src/app/code/community/FireGento/MageSetup/Helper/Contacts/Data.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * This file is part of a FireGento e.V. module.
+ *
+ * This FireGento e.V. module is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This script is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * PHP version 5
+ *
+ * @category  FireGento
+ * @package   FireGento_MageSetup
+ * @author    Julian Wittorf <jwittorf@die-netzwerkstatt.de>
+ * @copyright 2013-2016 FireGento Team (http://www.firegento.com)
+ * @license   http://opensource.org/licenses/gpl-3.0 GNU General Public License, version 3 (GPLv3)
+ * @version   2.2.4
+ * @since     2.2.4
+ */
+
+class FireGento_MageSetup_Helper_Contacts_Data
+    extends Mage_Contacts_Helper_Data
+{
+
+    const XML_PATH_ACCEPT_AGREEMENT   = 'contacts/contacts/accept_agreement';
+
+    /**
+     * Avoid loss of translation
+     */
+    public function __construct()
+    {
+        $this->_moduleName = 'Mage_Contacts';
+    }
+
+    public function isAcceptAgreement()
+    {
+        return Mage::getStoreConfig( self::XML_PATH_ACCEPT_AGREEMENT );
+    }
+}
